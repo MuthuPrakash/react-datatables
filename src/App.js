78da2f603fc12156e18b19369a8ac6cb6da9610c
muthu <muthu167@gmail.com>
2021-03-09 23:06:54 +0530

    new csv

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import Input from './Input';
 import Table from './Table';
 import Search from './search';
+import { CSVLink } from "react-csv";
 // var MongoClient = require('mongodb').MongoClient;
 
 class App extends Component {
@@ -13,6 +14,38 @@ class App extends Component {
         this.state = {
             names: itemsList
         };
+
+
+
+        this.headers = [
+            { label: "First Name", key: "firstName" },
+            { label: "Last Name", key: "lastName" },
+            { label: "Email", key: "email" },
+            { label: "Age", key: "age" }
+          ];
+           
+          this.data = [
+            { firstName: "Warren", lastName: "Morrow", email: "sokyt@mailinator.com", age: "36" },
+            { firstName: "Gwendolyn", lastName: "Galloway", email: "weciz@mailinator.com", age: "76" },
+            { firstName: "Astra", lastName: "Wyatt", email: "quvyn@mailinator.com", age: "57" },
+            { firstName: "Jasmine", lastName: "Wong", email: "toxazoc@mailinator.com", age: "42" },
+            { firstName: "Brooke", lastName: "Mcconnell", email: "vyry@mailinator.com", age: "56" },
+            { firstName: "Christen", lastName: "Haney", email: "pagevolal@mailinator.com", age: "23" },
+            { firstName: "Tate", lastName: "Vega", email: "dycubo@mailinator.com", age: "87" },
+            { firstName: "Amber", lastName: "Brady", email: "vyconixy@mailinator.com", age: "78" },
+            { firstName: "Philip", lastName: "Whitfield", email: "velyfi@mailinator.com", age: "22" },
+            { firstName: "Kitra", lastName: "Hammond", email: "fiwiloqu@mailinator.com", age: "35" },
+            { firstName: "Charity", lastName: "Mathews", email: "fubigonero@mailinator.com", age: "63" }
+          ];
+           
+          this.csvReport = {
+            data: this.data,
+            headers: this.headers,
+            filename: 'Clue_Mediator_Report.csv'
+          };
+
+          
+
 
         // var apiUrl = 'https://master-electricals.herokuapp.com/api/items';
         // fetch(apiUrl)
@@ -59,6 +92,7 @@ class App extends Component {
         // }
     }
 
+  
     onAddClick =  function(id, sysid, isreturn, returnid, booktype) {
         console.log('onAddClick line 19')
         let updated = false;
@@ -126,7 +160,7 @@ console.log('bookty[pe: ' +booktype);
 
     render() {
         return (
-            <div className="App">
+            <div className="App"> <CSVLink {...this.csvReport}>Export to CSV</CSVLink>
                 <Input onAddClick={(id, sysid, isreturn, returnid, type) => {
                     this.onAddClick(id, sysid, isreturn, returnid, type);
                 }} />
